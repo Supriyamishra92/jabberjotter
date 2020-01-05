21fba341fe6b7cbe386a4b39f2fc5ac84625ded0
Supriya Mishra <supriyamishra05992@gmail.com>
2020-01-05 20:38:30 +0530

    added basic speech-to-text feature in app.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:speech_recognition/speech_recognition.dart';
 
 void main() => runApp(JabberJotterApp());
 
@@ -7,35 +8,19 @@ class JabberJotterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'JabberJotter v1.0',
+      title: 'JabberJotter v0.1',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: JabberJotterHomePage(title: 'Welcome to JabberJotter.io'),
+
+      home: JabberJotterHomePage(
+        title: 'Welcome to JabberJotter.io'),
     );
   }
 }
 
 class JabberJotterHomePage extends StatefulWidget {
   JabberJotterHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +29,105 @@ class JabberJotterHomePage extends StatefulWidget {
 }
 
 class _JabberJotterHomePageState extends State<JabberJotterHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  SpeechRecognition _speech;
+  bool _speechRecognitionAvailable = false;
+  bool _isListening = false;
+
+  String transcription = '';
+
+  @override
+  void initState() {
+    super.initState();
+    activateSpeechRecognizer();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+
+    return Padding(
+        padding: new EdgeInsets.all(8.0),
+        child: new Center(
+          child: new Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              new Expanded(
+                  child: new Container(
+                      padding: const EdgeInsets.all(8.0),
+                      color: Colors.grey.shade200,
+                      child: new Text(transcription))),
+              _buildButton(
+                onPressed: _speechRecognitionAvailable && !_isListening
+                    ? () => start()
+                    : null,
+                label: _isListening
+                    ? 'Listening...'
+                    : 'Listen',
+              ),
+              _buildButton(
+                onPressed: _isListening ? () => cancel() : null,
+                label: 'Cancel',
+              ),
+              _buildButton(
+                onPressed: _isListening ? () => stop() : null,
+                label: 'Stop',
+              ),
+            ],
+          ),
+        ));
   }
+
+  void activateSpeechRecognizer() {
+    _speech = SpeechRecognition();
+
+    _speech.setAvailabilityHandler(onSpeechAvailability);
+    _speech.setRecognitionStartedHandler(onRecognitionStarted);
+    _speech.setRecognitionResultHandler(onrRecognitionResult);
+    _speech.setRecognitionCompleteHandler(onRecognitionComplete);
+
+    _speech.
+    activate()
+    .then((result)  => setState(() => _speechRecognitionAvailable = result));
+  }
+
+  void onSpeechAvailability(bool result) {
+    setState(() => _speechRecognitionAvailable = result);
+  }
+
+  void onRecognitionStarted() {
+    setState(() => _isListening = true);
+  }
+
+  void onrRecognitionResult(String text) {
+    setState(() => transcription = text);
+  }
+
+  void onRecognitionComplete() {
+    setState(() => _isListening = false);
+  }
+
+  void start() {
+    _speech.listen(locale: 'en_US');
+  }
+
+  void cancel() {
+    _speech.cancel().then((result) => setState(() => _isListening = result));
+  }
+
+  void stop() => _speech.stop().then((result) {
+    setState(() => _isListening = result);
+  });
 }
+
+Widget _buildButton({Function() onPressed, String label}) => new Padding(
+    padding: new EdgeInsets.all(12.0),
+    child: new RaisedButton(
+      color: Colors.cyan.shade600,
+      onPressed: onPressed,
+      child: new Text(
+        label,
+        style: const TextStyle(color: Colors.white),
+      ),
+    ));
+
